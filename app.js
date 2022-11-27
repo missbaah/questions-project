496848385b39dc9cfc6fa628138df91f683620ee
Adwoa Addo-Brako <baahbrako@gmail.com>
2022-11-27 23:56:11 +0000

    add JS functionality

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 //using selectors inside the element
 // traversing the dom
+
+displayBtn = document.querySelector(".question-btn");
+plusSign = document.querySelector(".plus-icon");
+minusSign = document.querySelector(".minus-icon");
+qtn = document.querySelectorAll(".question");
+
+console.log(displayBtn);
+
+displayBtn.addEventListener("click", () => {
+  qtn.forEach((e) => {
+    e.classList.add("show-text");
+  });
+});
